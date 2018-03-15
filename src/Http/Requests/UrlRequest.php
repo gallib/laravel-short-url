799f4294d2963f9f1e7ec887de187bd4b4f2ed74
Alain Pellaux <alain@gallib.net>
2018-03-15 06:35:53 +0100

    Remove useless check for prefix

--- src/Http/Requests/UrlRequest.php
+++ src/Http/Requests/UrlRequest.php
@@ -29,11 +29,9 @@ class UrlRequest extends FormRequest
             $uniqueCode .= ',id,'.$this->get('id');
         }
 
-        $codeNotIn = config('shorturl.route_form_prefix') ? '|not_in:'.config('shorturl.route_form_prefix') : '';
-
         return [
             'url'  => 'required|url',
-            'code' => 'max:255'.$uniqueCode.$codeNotIn,
+            'code' => 'max:255'.$uniqueCode,
         ];
     }
 
