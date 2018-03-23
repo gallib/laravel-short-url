240281c33de37abba6f0f77e88089026502d7f37
Alain Pellaux <alain@gallib.net>
2018-03-23 05:38:51 +0000

    Apply fixes from StyleCI

--- database/migrations/2018_03_22_160425_add_counter_to_urls.php
+++ database/migrations/2018_03_22_160425_add_counter_to_urls.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class AddCounterToUrls extends Migration
@@ -13,7 +12,7 @@ class AddCounterToUrls extends Migration
      */
     public function up()
     {
-        Schema::table('shorturl_urls', function($table) {
+        Schema::table('shorturl_urls', function ($table) {
             $table->unsignedInteger('counter')->default(0)->after('code');
         });
     }
