ca8ccdcd07a9b6fb37ed1731d35d71d605a48349
Alain Pellaux <alain@gallib.net>
2018-04-10 06:41:11 +0200

    Add a copy to clipboard button

--- resources/views/urls/index.blade.php
+++ resources/views/urls/index.blade.php
@@ -51,6 +51,7 @@
                                 <td><a href="{{ route('shorturl.redirect', $url->code) }}">{{ $url->code }}</a></td>
                                 <td>{{ $url->counter }}</td>
                                 <td>
+                                    <button class="btn btn-sm btn-success" data-clipboard-text="{{ route('shorturl.redirect', $url->code) }}">Copy</button>
                                     <a class="btn btn-sm btn-primary" href="{{ route('shorturl.url.edit', $url->id) }}" role="button">Edit</a>
                                     <form method="POST" action="{{ route('shorturl.url.destroy', $url->id) }}">
                                         @method('DELETE')
@@ -66,5 +67,13 @@
                 </div>
             </div>
         </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js"></script>
+        <script>
+            var clipboard = new ClipboardJS('.btn-success');
+
+            clipboard.on('success', function(e) {
+                e.trigger.innerText = 'Copied!';
+            });
+        </script>
     </body>
 </html>
