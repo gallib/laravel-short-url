5c9f77dc893a6229cd8468f3da4fd424bf0fb5d5
Alain Pellaux <alain@gallib.net>
2020-03-06 23:35:07 +0100

    Add user_id field

---
+++ database/migrations/2020_03_04_214540_add_user_id_to_urls.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserIdToUrls extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('shorturl_urls', function (Blueprint $table) {
+            $table->integer('user_id')->nullable()->unsigned()->after('expires_at');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+}
